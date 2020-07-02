7a65d048d0ba855bf2885fc6e7961026c31046ee
zachary mines <zmines@wesleyan.edu>
2020-07-02 03:39:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name=(name, breed )
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name 
+    @name 
+    @breed
+  end 
+end 
